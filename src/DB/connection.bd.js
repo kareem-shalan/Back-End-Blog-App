28a523a02b2a38de2b0c3d97797aa62bd85b90ac
kareem-shalan <kareemmohamedali330@gamil.com>
2025-06-25 03:28:26 +0300

    link mysql from clever cloud

--- src/DB/connection.bd.js
+++ src/DB/connection.bd.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('blogAppSequelize', 'root', '', {
-    host: 'localhost',
-    dialect: "mysql"
+export const sequelize = new Sequelize('bjvisvfwlrb5lmjos4mr', 'uglvgflnshesl1yz', 'gCCTIZdq8NaUzHbt96rM', {
+    host: 'bjvisvfwlrb5lmjos4mr-mysql.services.clever-cloud.com',
+    dialect: "mysql",
+    port: 3306,
 })
 
 export async function checkDBconnection() {
@@ -17,7 +18,7 @@ export async function checkDBconnection() {
 
 export const syncDBconnection = async () => {
     try {
-        await sequelize.sync({ alter: false , force: false })
+        await sequelize.sync({ alter: false, force: false })
         console.log('Database synchronized successfully.');
     } catch (error) {
         console.log("😪😪😪😪😪😪 Database sync failed:", error);
